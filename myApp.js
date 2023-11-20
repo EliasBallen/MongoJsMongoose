85d76f6f67ecf10a883af7234fdf45d279d0ba1f
EliasBallen <@EliasBallen>
2023-11-20 09:01:05 -0500

    add the mongo uri to the .env file and connect to mongodb atlas via mongoose

--- myApp.js
+++ myApp.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
-
+const mongoose = require('mongoose');
+//mongodb+srv://MongoTestUserName:MongoTestUserPasswordForApp@jsmongosseapp.yzjwrgb.mongodb.net/?retryWrites=true&w=majority
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
